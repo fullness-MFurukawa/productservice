e772ed94860b7e3b2166f586ed1d4d6d3abb5591
FullnessMFurukawa <furukawa@fullness.co.jp>
2023-03-29 09:21:22 +0000

    2023-03-29

--- presentation/gin/sserver_test.go
+++ presentation/gin/sserver_test.go
@@ -25,3 +25,20 @@ func TestList(t *testing.T) {
 	// レスポンスステータスの評価
 	assert.Equal(t, 200, recoder.Code)
 }
+
+func TestSearchKeyword(t *testing.T) {
+	// SqlBuilderのコネクションプールを生成
+	sqlboiler.NewSqlBiolderInitDB().Init(nil)
+	// Serverのセットアップ
+	router := SetupGinServer()
+
+	// ResponseRecoderの生成
+	recoder := httptest.NewRecorder()
+	// テストリクエスト:商品一覧取得の生成
+	req, _ := http.NewRequest("GET", "/product/search/ペン", nil)
+	// テストリクエストの送信
+	router.ServeHTTP(recoder, req)
+
+	// レスポンスステータスの評価
+	assert.Equal(t, 200, recoder.Code)
+}
